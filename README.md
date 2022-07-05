2863efd18742feb23cfa7391e639782b70438c0e
Thomas Deppisch <thomas.deppisch@posteo.de>
2022-07-05 11:57:47 +0200

    add readme, license

---
+++ README.md
@@ -0,0 +1,15 @@
+# Direct and Residual Subspace Decomposition for Spatial Room Impulse Responses
+
+This repository contains a MATLAB implementation of the direct and residual subspace decomposition for spatial room impulse responses (SRIRs). The method decomposes SRIRs into a direct part, comprising direct sound and salient reflections, and a residual.
+
+For more information and if you want to reference the code please refer to
+   
+   ```
+   T. Deppisch, S. Amengual Garí, P. Calamia, and J. Ahrens, "Direct and Residual Subspace Decomposition of Spatial Room Impulse Responses," ADD INFORMATION, 2022.
+   ```
+   
+## Acknowledgment
+We thank Meta Reality Labs Research for funding this project.
+
+## License
+This software is licensed under a Non-Commercial Software License (see [LICENSE](https://github.com/thomasdeppisch/SRIR-Subspace-Decomposition/blob/master/LICENSE) for full details).
--- testSrirSubspaceDecomp.m
+++ testSrirSubspaceDecomp.m
@@ -2,16 +2,13 @@ clear all
 close all
 
 %% test the direct and residual subspace decomposition of an SRIR
-rirStruct = load('TA_lecture_hall_em32_Genelec_S1_R1.mat');
-srir = rirStruct.irs;
-fs = rirStruct.fs;
+srirStruct = load('resources/TA_lecture_hall_em32_ACN_N3D.mat');
+srir = srirStruct.srir;
+fs = srirStruct.fs;
 
 shOrder = 4;
-% TODO: remove dependency on this function
-srirShdFiltered = encodeMicArray(srir, fs, true, shOrder=shOrder);
 
-% parameters for the subspace decomposition, see the function header for details:
-% TODO: explain what the parameters do and how they behave
+% parameters for the subspace decomposition, see the function header of srirSubspaceDecomp for details
 blockLenSmp = 32;
 hopSizeSmp = blockLenSmp / 8;
 noiseAnalysisTimeMs = 20;
@@ -21,11 +18,11 @@ numBlocksGsvSumAvg = 32;
 kappa = 3;
 
 [dirSrir, resSrir, numDirSubspaceComponents, gsvs, detectionThreshold, gsvSum, avgGsvSum] = ...
-            srirSubspaceDecomp(srirShdFiltered, fs, blockLenSmp, hopSizeSmp, noiseAnalysisTimeMs, decompositionTimeLimitMs, ...
+            srirSubspaceDecomp(srir, fs, blockLenSmp, hopSizeSmp, noiseAnalysisTimeMs, decompositionTimeLimitMs, ...
             numBlocksSmoothThresh, numBlocksGsvSumAvg, kappa);
 
 %% plot
-srirLen = size(srirShdFiltered,1);
+srirLen = size(srir,1);
 t = linspace(0, srirLen/fs-1/fs, srirLen).';
 tBlocks = (0:hopSizeSmp:size(gsvs,1)*hopSizeSmp-hopSizeSmp)/fs;
 
@@ -40,7 +37,7 @@ ylabel('$\| \cdot \|$ (dB)')
 legend('$\mathbf{x}_\mathrm{d}(t)$', '$\mathbf{x}_\mathrm{r}(t)$')
 grid on
 
-numChannels = size(srirShdFiltered,2);
+numChannels = size(srir,2);
 cumsumGSVsColors = copper(numChannels);
 cumsumGSVs = cumsum(gsvs,2);
 
